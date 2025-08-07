8e02af8881dc79ffe1622b3bf73adfc473c1ee90
Wilhelm Woelkner <wilhelm.woelkner@outlook.de>
2025-08-07 10:59:37 +0200

    add draft for host.nix

--- hosts/laptopUni.nix
+++ hosts/laptopUni.nix
@@ -1,1 +1,27 @@
 # host configuration for laptopUni
+
+{ config, pkgs, ...}: {
+  imports = [];
+
+  nixppkgs.config.allowUnfree = true;
+  
+  networking.hostName = "laptopUni";
+  time.timeZone = "Europe/Berlin";
+
+  users.users.willi = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "audio" "input" ];
+  };
+
+  enviromnemt.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
+  services.xserver.enable = false;
+  services.displayManager.enable = false;
+  console.useXkbConfig = true;
+
+  programs.hyprland.enable = true;
+
+  system.stateVersion = "25.05";
