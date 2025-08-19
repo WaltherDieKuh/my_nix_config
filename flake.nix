3d55c65087b4ac9db57836065ab44f6ff43af9df
Wilhelm Wölkner <wilhelm.woelkner@outlook.de>
2025-08-19 14:54:37 +0200

    #include hardware config

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
       laptopUni = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	modules = [
+	  ./hosts/hardware-configuration.nix
 	  ./hosts/laptopUni.nix
 	  home-manager.nixosModules.home-manager{
  	    home-manager.useGlobalPkgs = true;
--- hosts/laptopUni.nix
+++ hosts/laptopUni.nix
@@ -7,22 +7,6 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # File systems configuration. You need to edit these to match your partitions.
-  # Use `lsblk` or a similar tool to find your device names.
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/05c0a8ea0-bf58-4109-b810-c920db6d916b";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/5FCD-DF79"; # or "/dev/sda1"
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
-  };
-  
-  swapDevices = 
-  [{device = "/dev/disk/by-uuid/89f0c0dc-d742ö-4546-853f-c01c3789dd99";}];
   
   networking.hostName = "laptopUni";
   time.timeZone = "Europe/Berlin";
