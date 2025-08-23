40cbcef3c5ebf2e6afd3d53eb1531defe250fbd1
Wilhelm Woelkner <wilhelm.woelkner@outlook.de>
2025-08-23 11:06:31 +0200

    fix syntax

--- flake.nix
+++ flake.nix
@@ -19,10 +19,11 @@
 	  home-manager.nixosModules.home-manager{
  	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
-	    home-manager.users.willi = imports = [
-              ./home/willi.nix;
-              ./modules/default.nix;
+	    home-manager.users.willi = { imports = [
+              ./home/willi.nix
+              ./modules/default.nix
             ];
+          };
 	  }
 	  hyprland.nixosModules.default
 	];
