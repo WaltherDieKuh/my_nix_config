d1046f0c8ed47fd486ee535900f39e7aa1880775
Wilhelm Woelkner <wilhelm.woelkner@outlook.de>
2026-04-07 15:01:11 +0200

    put magicq overlay in seperate file

--- flake.nix
+++ flake.nix
@@ -69,62 +69,9 @@
           #hyprland.nixosModules.default
 
           # Das Overlay-Modul wird hier als letztes Element eingefügt
-          (
-            {
-              config,
-              pkgs,
-              ...
-            }: {
-              nixpkgs.overlays = [
-                (final: prev: {
-                  magicq = prev.callPackage ./pkgs/magicq.nix {};
-                  libsForQt5 =
-                    prev.libsForQt5
-                    // {
-                      # Leite die alte Anfrage an den neuen Ort weiter
-                      layer-shell-qt = final.kdePackages.layer-shell-qt;
-                    };
-
-                  # Bonus: Manche alten Pakete suchen vielleicht auch hier.
-                  # Dies ist optional, aber sicher ist sicher.
-                  layer-shell-qt = final.kdePackages.layer-shell-qt;
-                  minecraft-plymouth-theme = prev.stdenv.mkDerivation {
-                    pname = "minecraft-plymouth-theme";
-                    version = "git";
-                    src = inputs.minecraft-plymouth-theme;
-
-                    installPhase = ''
-                      runHook preInstall
-
-                      # Zielverzeichnis im Store anlegen
-                      local theme_dir="$out/share/plymouth/themes/minecraft"
-                      mkdir -p "$theme_dir"
-
-                      # Quell-Verzeichnis kopieren
-                      cp -r ./plymouth/* "$theme_dir/"
-
-                      # Dateien umbenennen, um der Plymouth-Konvention zu entsprechen (theme_name/theme_name.plymouth)
-                      mv "$theme_dir/mc.plymouth" "$theme_dir/minecraft.plymouth"
-                      mv "$theme_dir/mc.script" "$theme_dir/minecraft.script"
-
-                      # Pfade innerhalb der .plymouth-Datei korrigieren, damit sie auf die neuen Dateinamen zeigen
-                      sed -i 's|/mc.script|/minecraft.script|g' "$theme_dir/minecraft.plymouth"
-                      sed -i 's|/themes/minecraft|/themes/minecraft|g' "$theme_dir/minecraft.plymouth" # Stellt sicher, dass der Pfad stimmt
-
-                      runHook postInstall
-                    '';
-
-                    meta = with prev.lib; {
-                      description = "A Plymouth theme inspired by Minecraft";
-                      homepage = "https://github.com/nikp123/minecraft-plymouth-theme";
-                      license = licenses.unlicense;
-                      platforms = platforms.linux;
-                    };
-                  };
-                })
-              ];
-            }
-          )
+          ({ config, pkgs, ... }: {
+            nixpkgs.overlays = [ overlays.custom ];
+          })
         ];
       };
 
@@ -160,62 +107,9 @@
           #hyprland.nixosModules.default
 
           # Das Overlay-Modul wird hier als letztes Element eingefügt
-          (
-            {
-              config,
-              pkgs,
-              ...
-            }: {
-              nixpkgs.overlays = [
-                (final: prev: {
-                  magicq = prev.callPackage ./pkgs/magicq.nix {};
-                  libsForQt5 =
-                    prev.libsForQt5
-                    // {
-                      # Leite die alte Anfrage an den neuen Ort weiter
-                      layer-shell-qt = final.kdePackages.layer-shell-qt;
-                    };
-
-                  # Bonus: Manche alten Pakete suchen vielleicht auch hier.
-                  # Dies ist optional, aber sicher ist sicher.
-                  layer-shell-qt = final.kdePackages.layer-shell-qt;
-                  minecraft-plymouth-theme = prev.stdenv.mkDerivation {
-                    pname = "minecraft-plymouth-theme";
-                    version = "git";
-                    src = inputs.minecraft-plymouth-theme;
-
-                    installPhase = ''
-                      runHook preInstall
-
-                      # Zielverzeichnis im Store anlegen
-                      local theme_dir="$out/share/plymouth/themes/minecraft"
-                      mkdir -p "$theme_dir"
-
-                      # Quell-Verzeichnis kopieren
-                      cp -r ./plymouth/* "$theme_dir/"
-
-                      # Dateien umbenennen, um der Plymouth-Konvention zu entsprechen (theme_name/theme_name.plymouth)
-                      mv "$theme_dir/mc.plymouth" "$theme_dir/minecraft.plymouth"
-                      mv "$theme_dir/mc.script" "$theme_dir/minecraft.script"
-
-                      # Pfade innerhalb der .plymouth-Datei korrigieren, damit sie auf die neuen Dateinamen zeigen
-                      sed -i 's|/mc.script|/minecraft.script|g' "$theme_dir/minecraft.plymouth"
-                      sed -i 's|/themes/minecraft|/themes/minecraft|g' "$theme_dir/minecraft.plymouth" # Stellt sicher, dass der Pfad stimmt
-
-                      runHook postInstall
-                    '';
-
-                    meta = with prev.lib; {
-                      description = "A Plymouth theme inspired by Minecraft";
-                      homepage = "https://github.com/nikp123/minecraft-plymouth-theme";
-                      license = licenses.unlicense;
-                      platforms = platforms.linux;
-                    };
-                  };
-                })
-              ];
-            }
-          )
+          ({ config, pkgs, ... }: {
+            nixpkgs.overlays = [ overlays.custom ];
+          })
         ];
       };
     };
--- overlays/default.nix
+++ overlays/default.nix
@@ -8,4 +8,35 @@
       config.allowUnfree = true;
     };
   };
+  custom = final: prev: {
+    magicq = prev.callPackage ../pkgs/magicq.nix {};
+    libsForQt5 = prev.libsForQt5 // {
+      layer-shell-qt = final.kdePackages.layer-shell-qt;
+    };
+    layer-shell-qt = final.kdePackages.layer-shell-qt;
+    minecraft-plymouth-theme = prev.stdenv.mkDerivation {
+      pname = "minecraft-plymouth-theme";
+      version = "git";
+      src = inputs.minecraft-plymouth-theme;
+
+      installPhase = ''
+        runHook preInstall
+        local theme_dir="$out/share/plymouth/themes/minecraft"
+        mkdir -p "$theme_dir"
+        cp -r ./plymouth/* "$theme_dir/"
+        mv "$theme_dir/mc.plymouth" "$theme_dir/minecraft.plymouth"
+        mv "$theme_dir/mc.script" "$theme_dir/minecraft.script"
+        sed -i 's|/mc.script|/minecraft.script|g' "$theme_dir/minecraft.plymouth"
+        sed -i 's|/themes/minecraft|/themes/minecraft|g' "$theme_dir/minecraft.plymouth"
+        runHook postInstall
+      '';
+
+      meta = with prev.lib; {
+        description = "A Plymouth theme inspired by Minecraft";
+        homepage = "https://github.com/nikp123/minecraft-plymouth-theme";
+        license = licenses.unlicense;
+        platforms = platforms.linux;
+      };
+    };
+  };
 }
